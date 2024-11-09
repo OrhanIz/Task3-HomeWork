264f03baca2dc91628a5f7f1e013e09c0c2be292
OrhanIz <izmiro4914730_aehit@students.vizja.pl>
2024-11-10 00:11:37 +0100

    SearchAlgorithms
    This project implements linear and binary search algorithms on both vector and array data structures. It includes functions for standard search as well as finding the first occurrence of an element.

---
+++ SearchingApp1/SearchingApp1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "Searching.h"
+
+int main() {
+    std::vector<int> vec = { 10, 20, 30, 30, 30, 40, 50, 60, 70, 70 };
+    int target;
+
+    std::cout << "Enter target value: ";
+    std::cin >> target;
+
+    int index = binarySearch(vec, target);
+    if (index != -1) {
+        std::cout << "Binary search. Element found at index: " << index << std::endl;
+    }
+    else {
+        std::cout << "Binary search. Element not found in the vector." << std::endl;
+    }
+
+    index = binarySearchFirst(vec, target);
+    if (index != -1) {
+        std::cout << "Binary search first occurrence. Element found at index: " << index << std::endl;
+    }
+    else {
+        std::cout << "Binary search first occurrence. Element not found in the vector." << std::endl;
+    }
+
+    index = linearSearch(vec, target);
+    if (index != -1) {
+        std::cout << "Linear search. Element found at index: " << index << std::endl;
+    }
+    else {
+        std::cout << "Linear search. Element not found in the vector." << std::endl;
+    }
+
+    return 0;
+}
